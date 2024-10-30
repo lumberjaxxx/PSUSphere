79c1d23f8a38746e962b6ba1a8350d129c0e85ae
sci17 <recarzekaycee@gmail.com>
2024-10-30 21:13:37 +0700

    added college into org,student, and program

--- projectsite/psusphere/models.py
+++ projectsite/psusphere/models.py
@@ -16,14 +16,14 @@ class College(BaseModel):
 
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150)
-    college = models.ForeignKey(College, on_delete=models.CASCADE, related_name="programs")
+    college = models.ForeignKey(College, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.prog_name
 
 class Organization(BaseModel):
     name = models.CharField(max_length=250)
-    college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE, related_name="organizations")
+    college = models.ForeignKey(College, null=True, blank=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
 
     def __str__(self):
@@ -34,14 +34,15 @@ class Student(BaseModel):
     lastname = models.CharField(max_length=25)
     firstname = models.CharField(max_length=25)
     middlename = models.CharField(max_length=25, blank=True, null=True)
-    program = models.ForeignKey(Program, on_delete=models.CASCADE, related_name="students")
+    program = models.ForeignKey(Program, on_delete=models.CASCADE)
+    college = models.ForeignKey(College, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f"{self.lastname}, {self.firstname}"
 
 class OrgMember(BaseModel):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name="memberships")
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="members")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     date_joined = models.DateField()
 
     def __str__(self):
