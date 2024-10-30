0fba3e167a22b1180ffc7295276a09cf56ab0ea5
Rex Gatchalian <rexg744@gmail.com>
2024-10-30 14:13:19 +0800

    assets added

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from psusphere.views import HomePageView
+from psusphere import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomePageView.as_view(), name="home"),
 ]
