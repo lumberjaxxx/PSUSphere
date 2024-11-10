4b36e23774e4ef343cca0567e63b8c8a118a0da4
Rex Gatchalian <rexg744@gmail.com>
2024-11-10 08:52:46 +0800

    modifications on models

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, re_path
 from psusphere import views
 from django.contrib.auth import views as auth_views
-from psusphere.views import HomePageView, OrganizationList, OrganizationCreateView
+from psusphere.views import HomePageView, OrganizationList, OrganizationCreateView, OrgMemberList, StudentList, ProgramList, CollegeList
 
 
 urlpatterns = [
@@ -27,5 +27,10 @@ urlpatterns = [
     path('orglist/', OrganizationList.as_view(), name='org_list'),
     path('orglist/add/', OrganizationCreateView.as_view(), name='org_add'),
     re_path(r'^login/$',auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout')
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    path('org_mem/', OrgMemberList.as_view(), name="org_mem"),
+    path('student/', StudentList.as_view(), name="student"),
+    path('program/', ProgramList.as_view(), name='program'),
+    path('college', CollegeList.as_view(), name='college')
+
 ]
