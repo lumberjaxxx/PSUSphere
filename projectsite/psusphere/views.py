90bb47297410fce9f57fdc5207d2bd61a6fbc5ad
sci17 <recarzekaycee@gmail.com>
2024-11-18 21:46:26 +0700

    modified for the search

--- projectsite/psusphere/views.py
+++ projectsite/psusphere/views.py
@@ -37,11 +37,27 @@ class OrganizationCreateView(CreateView):
     template_name = "org_form.html"
     success_url = reverse_lazy('org_list')
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrganizationCreateView, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q') != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query))
+            
+        return qs
+
 class OrgMemberList(ListView):
     model = OrgMember
     context_object_name = "org_mem"
     template_name = "org_mem.html"
     paginated = 2
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(OrgMemberList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q') != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query))
+            
+        return qs
 
 class StudentList(ListView):
     model = Student
@@ -49,14 +65,38 @@ class StudentList(ListView):
     template_name = "student.html"
     paginated = 5
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(StudentList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q') != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query))
+            
+        return qs
+
 class CollegeList(ListView):
     model = College
     context_object_name = "college"
     template_name = "college.html"
     paginated = 3
 
+    def get_queryset(self, *args, **kwargs):
+        qs = super(CollegeList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q') != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query))
+            
+        return qs
+
 class ProgramList(ListView):
     model = Program
     context_object_name = "program"
     template_name = "program.html"
     paginated = 2
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(ProgramList, self).get_queryset(*args, **kwargs)
+        if self.request.GET.get('q') != None:
+            query = self.request.GET.get('q')
+            qs = qs.filter(Q(name__icontains=query) | Q(description__icontains=query))
+            
+        return qs
