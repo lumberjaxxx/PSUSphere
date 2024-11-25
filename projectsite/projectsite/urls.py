4056f7e712e10ce42545a9c32da5ca79ee5027de
Rex Gatchalian <rexg744@gmail.com>
2024-11-25 14:43:50 +0800

    added forms

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -18,19 +18,28 @@ from django.contrib import admin
 from django.urls import path, re_path
 from psusphere import views
 from django.contrib.auth import views as auth_views
-from psusphere.views import HomePageView, OrganizationList, OrganizationCreateView, OrgMemberList, StudentList, ProgramList, CollegeList
+from psusphere.views import HomePageView, OrganizationList, OrganizationCreateView, OrgMemberList, StudentList, ProgramList, CollegeList, OrgMemberCreateView, StudentCreateView, ProgramCreateView, CollegeCreateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HomePageView.as_view(), name="home"),
+    path('', views.HomePageView.as_view(), name="home"), #home
+
     path('orglist/', OrganizationList.as_view(), name='org_list'),
-    path('orglist/add/', OrganizationCreateView.as_view(), name='org_add'),
+    path('orglist/add/', OrganizationCreateView.as_view(), name='org_add'),  ##### adding in forms
+
     re_path(r'^login/$',auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    path('org_mem/', OrgMemberList.as_view(), name="org_mem"),
-    path('student/', StudentList.as_view(), name="student"),
-    path('program/', ProgramList.as_view(), name='program'),
-    path('college', CollegeList.as_view(), name='college')
 
+    path('org_mem/', OrgMemberList.as_view(), name="org_mem"),
+    path('orgmem/add/', OrgMemberCreateView.as_view(), name='orgmem_add'),
+
+    path('student/', StudentList.as_view(), name="student"),
+    path('student/add/', StudentCreateView.as_view(), name='student_add'),
+
+    path('program/', ProgramList.as_view(), name='program'),
+    path('program/add/', ProgramCreateView.as_view(), name='program_add'), 
+
+    path('college', CollegeList.as_view(), name='college'),
+    path('college/add/', CollegeCreateView.as_view(), name='college_add')
 ]
